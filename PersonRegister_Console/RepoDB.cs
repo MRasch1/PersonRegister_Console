deeff6df0478b29fc60d1a6653dd73ec5988e8a6
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2023-10-13 14:16:51 +0200

    Add project files.

---
+++ PersonRegister_Console/RepoDB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonRegister_Console
+{
+    internal class RepoDB
+    {
+        string _connectionString = "Data Source=192.168.23.123;Initial Catalog=PersonRegister;User ID=sa;Password=Ab1234";
+
+        public void InsertPersonIRegister(string fornavn, string efternavn, DateTime fødselsdato)
+        {
+            string insertQuery = "INSERT INTO Person (Fornavn, Efternavn, Fødselsdato) VALUES (@Fornavn, @Efternavn, @Fødselsdato";
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand(insertQuery, conn))
+                {
+                    sqlCommand.Parameters.AddWithValue("@Fornavn", fornavn);
+                    sqlCommand.Parameters.AddWithValue("@Efternavn", efternavn);
+                    sqlCommand.Parameters.AddWithValue("@Fødselsdato", fødselsdato);
+
+                    conn.Open();
+
+                    int rowsAffected = sqlCommand.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Row inserted successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to insert the row");
+                    }
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ PersonRegister_Console/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonRegister_Console
+{
+    internal class Startup
+    {
+        public Startup()
+        {
+            Console.WriteLine("Registrer en person");
+            Console.Write("Skriv venligst fornavn: ");
+            string fornavn = Console.ReadLine();
+            Console.Write("Skriv venligst efternavn: ");
+            string efternavn = Console.ReadLine();
+            Console.Write("Skriv Fødselsdato med følgende format DD-MM-YYYY: ");
+            DateTime fødselsdato = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(fornavn + " " + efternavn + " " + fødselsdato.Date.Year);
+            Console.ReadKey();
+
+        }
+
+    }
+}
